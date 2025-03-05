e7f1bbb11c22cc6eb0663ec191ade6702e824200
Hari <hariprakashslmom@gmail.com>
2025-03-05 21:50:43 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useActionState, useCallback, useState } from "react";
 
+interface IUser {
+  name: string;
+  dob: string;
+  role: string;
+}
+
+function UserForm({
+  onUserAdd,
+}: {
+  onUserAdd: (prevState: any, queryData: FormData) => void;
+}) {
+  const [message, formAction, isPending] = useActionState(onUserAdd, null);
+  return (
+    <form action={formAction}>
+      <div>
+        <label htmlFor="user-name">Name:</label>{" "}
+        <input type="text" name="name" id="user-name" />
+      </div>
+      <div>
+        <label htmlFor="user-dob">DOB:</label>{" "}
+        <input type="date" name="dob" id="user-dob" />
+      </div>
+      <div>
+        <label htmlFor="user-role">Role:</label>{" "}
+        <input type="text" name="role" id="user-role" />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+}
 export default function Home() {
+  const [users, setUser] = useState<IUser[]>([]);
+
+  const onUserAdd = useCallback((prevState: any, queryData: FormData) => {
+    const user: IUser = {
+      name: queryData.get("name") as string,
+      dob: queryData.get("dob") as string,
+      role: queryData.get("role") as string,
+    };
+    setUser((prevState) => [...prevState, user]);
+  }, []);
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <UserForm onUserAdd={onUserAdd} />
+      <table>
+        <thead>
+          <th> Name</th>
+          <th>DOB</th>
+          <th>Role</th>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.dob}</td>
+                <td>{user.role}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
